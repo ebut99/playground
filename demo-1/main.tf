0de15703e1db2a5194eefe5c7dbc8ffa4b0ba3c1
Pramod <ebut99@gmail.com>
2020-05-17 22:49:21 -0400

    updates

--- demo-1/main.tf
+++ demo-1/main.tf
@@ -1,15 +1,44 @@
 provider "aws" {
-  region = var.AWS_REGION
+  region     = var.AWS_REGION
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECERT_KEY
 }
 
+data "aws_ami" "t2_ami" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "t2_keypair" {
+  key_name   = "t2_keypair"
+  public_key = var.public_key
+  tags = {
+    Name = "t2_keypair"
+  }
+}
+
 resource "aws_instance" "myfirstinstance" {
-  ami           = "ami-0a040c35ca945058a"
+  ami           = aws_ami.t2_ami.id
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.t2_keypair.id
 
   tags = {
-      Name = "first demo"
+    Name = "first demo"
   }
 
 }
--- demo-1/variables.tf
+++ demo-1/variables.tf
@@ -2,5 +2,7 @@ variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECERT_KEY" {}
 
 variable "AWS_REGION" {
-    default = "us-east-2"
+  default = "us-east-2"
 }
+
+variable "public_key" {}
